a3935006966d5a840d515ee4fcb045af3050f4fb
Ricardo Ribeiro <ricardo.ribeiro@neuro.fchampalimaud.org>
2016-02-22 18:14:55 +0000

    LIPHPCEnvManagerAppThunder.py edited online with Bitbucket

--- server/opencsp/envmanagers/LIPHPCEnvManager/LIPHPCEnvManagerAppThunder.py
+++ server/opencsp/envmanagers/LIPHPCEnvManager/LIPHPCEnvManagerAppThunder.py
@@ -40,6 +40,6 @@ class LIPHPCEnvManagerAppThunder(LIPHPCEnvManager):
 
         mkdir output
         echo {0} > busy.no
-        """.format(job.pk ,jobparamsp['_numberofmachines'],jobparamsp['_scriptfile']).replace('\t\t\t','')
+        """.format(job.pk ,jobparamsp['_numberofmachines']['position'],'input'+jobparamsp['_scriptfile']).replace('\t\t\t','')
 
         return command
